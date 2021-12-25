c25a50740c11cd5c659fbd72daf63e4da2d41762
Gennady Podrezov <gennady@gennadys-air.myfiosgateway.com>
2021-12-24 21:18:25 -0500

    I created second model for Posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    text = models.TextField(default="Add your text here")
+    image = models.ImageField(upload_to='events_images/')
